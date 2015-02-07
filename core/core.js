67dc6264e37a7da3054aced95dd2cd564b0ffcac
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-02-07 13:43:29 +0300

    derived context from object, added positional properties, cleaned up remaining returns, added simple anchor test

--- core/core.js
+++ core/core.js
@@ -44,64 +44,125 @@ _globals.core.Object.prototype.get = function (name) {
 function setup(context) {
 	_globals.core.Item.prototype.children = []
 
+	_globals.core.Item.prototype.toScreen = function() {
+		var item = this;
+		var x = 0, y = 0;
+		while(item) {
+			console.log(item, item.x, item.y, x, y);
+			x += item.x;
+			y += item.y;
+			item = item.parent;
+		}
+		return [x, y, x + this.width, y + this.height];
+	}
+
 	_globals.core.Border.prototype._update = function(name, value) {
 		switch(name) {
-			case 'width': this.parent.element.css('border-width', value); return;
-			case 'color': this.parent.element.css('border-color', value); return;
+			case 'width': this.parent.element.css('border-width', value); break;
+			case 'color': this.parent.element.css('border-color', value); break;
 		}
 		_globals.core.Object.prototype._update.apply(this, arguments);
 	}
 
 	_globals.core.Item.prototype._update = function(name, value) {
 		switch(name) {
-			case 'width': 	this.element.css('width', value); this.right.value = this.x + value; return;
-			case 'height':	this.element.css('height', value); this.bottom.value = this.y + value; return;
-			case 'x':		this.element.css('left', value); this.left.value = value; this.right.value = value + this.width; return;
-			case 'y':		this.element.css('top', value); this.top.value = value; this.bottom.value = value + this.height; return;
-			case 'radius':	this.element.css('border-radius', value); return;
+			case 'width': 	this.element.css('width', value); this.right.value = this.x + value; break;
+			case 'height':	this.element.css('height', value); this.bottom.value = this.y + value; break;
+			case 'x':		this.element.css('left', value); this.left.value = value; this.right.value = value + this.width; break;
+			case 'y':		this.element.css('top', value); this.top.value = value; this.bottom.value = value + this.height; break;
+			case 'radius':	this.element.css('border-radius', value); break;
+		}
+		_globals.core.Object.prototype._update.apply(this, arguments);
+	}
+
+	_globals.core.AnchorLine.prototype._update = function(name, value) {
+		_globals.core.Object.prototype._update.apply(this, arguments);
+	}
+
+	_globals.core.Anchors.prototype._update = function(name, line) {
+		var self = this.parent;
+		var target = line.parent;
+		var anchors = this;
+		var lm = anchors.leftMargin || anchors.margins;
+		var rm = anchors.rightMargin || anchors.margins;
+
+		var update_left = function() {
+			console.log("left.changed");
+			var target_box = target.toScreen();
+			var parent_box = self.parent.toScreen();
+			console.log(target_box, parent_box);
+		};
+
+		var update_right = function() {
+			console.log("right.changed");
+		};
+
+		switch(name) {
+			case 'left':
+				update_left(target.left.value);
+				target.left.onChanged('value', update_left);
+				break;
+
+			case 'right':
+				update_right(target.right.value);
+				target.right.onChanged('value', update_left);
+				break;
 		}
 		_globals.core.Object.prototype._update.apply(this, arguments);
 	}
 
 	_globals.core.Font.prototype._update = function(name, value) {
 		switch(name) {
-			case 'pointSize': this.parent.element.css('font-size', value + "pt"); return;
-			case 'pixelSize': this.parent.element.css('font-size', value + "px"); return;
-			case 'italic': this.parent.element.css('font-style', value? 'italic': 'normal'); return;
+			case 'pointSize': this.parent.element.css('font-size', value + "pt"); break;
+			case 'pixelSize': this.parent.element.css('font-size', value + "px"); break;
+			case 'italic': this.parent.element.css('font-style', value? 'italic': 'normal'); break;
 		}
 		_globals.core.Object.prototype._update.apply(this, arguments);
 	}
 
 	_globals.core.Text.prototype._update = function(name, value) {
 		switch(name) {
-			case 'text': this.element.text(value); return;
+			case 'text': this.element.text(value); break;
 		}
 		_globals.core.Item.prototype._update.apply(this, arguments);
 	}
 
 	_globals.core.Rectangle.prototype._update = function(name, value) {
 		switch(name) {
-			case 'color': this.element.css('background-color', value); return;
+			case 'color': this.element.css('background-color', value); break;
 		}
 		_globals.core.Item.prototype._update.apply(this, arguments);
 	}
 }
 
 exports.Context = function() {
-	var windowW = $(window).width();
-	var windowH = $(window).height();
-	console.log("window size: " + windowW + "x" + windowH);
+	_globals.core.Object.apply(this, null);
+
+	var w = $(window).width();
+	var h = $(window).height();
+	console.log("window size: " + w + "x" + h);
+
 	var body = $('body');
 	var div = $("<div id='renderer'></div>");
 	body.append(div)
-	//fixme: derive context from Object
-	this._local = {}
-	this.element = div
-	this.element.css({width: windowW, height: windowH});
-	console.log("context created");
 
+	exports.addProperty(this, 'int', 'x');
+	exports.addProperty(this, 'int', 'y');
+	exports.addProperty(this, 'int', 'width');
+	exports.addProperty(this, 'int', 'height');
+
+	this.element = div
+	this.element.css({width: w, height: h});
+	this.width = w;
+	this.height = h;
+
+	console.log("context created");
 	setup(this);
 }
+
+exports.Context.prototype = Object.create(_globals.core.Object.prototype);
+exports.Context.prototype.constructor = exports.Context;
+
 
 exports.Context.prototype.start = function(name) {
 	var proto;

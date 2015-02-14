1fce0efcea5b095f361d8dae51099ab07266feae
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-02-14 11:01:23 +0300

    added opacity handling

--- core/core.js
+++ core/core.js
@@ -166,6 +166,7 @@ exports._setup = function() {
 				this.verticalCenter.value = (this.top.value + this.bottom.value) / 2;
 				break;
 
+			case 'opacity': if (this.element) /*FIXME*/this.element.css('opacity', value); break;
 			case 'visible':	if (this.element) /*FIXME*/this.element.css('visibility', value? 'visible': 'hidden'); break;
 			case 'z':		this.element.css('z-index', value); break;
 			case 'radius':	this.element.css('border-radius', value); break;

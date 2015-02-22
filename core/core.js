5126e69c5f286e42ddf7120ac27ccc3fcf27d423
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-02-22 21:57:15 +0300

    switched to prefixed version of linear gradient

--- core/core.js
+++ core/core.js
@@ -685,10 +685,10 @@ exports._setup = function() {
 				if (value) {
 					var decl = value._getDeclaration()
 					this.element.css('background-color', '')
-					this.element.css('background', 'linear-gradient(to ' + decl + ')')
+					//this.element.css('background', 'linear-gradient(to ' + decl + ')')
 					this.element.css('background', '-o-linear-gradient(' + decl + ')')
 					this.element.css('background', '-moz-linear-gradient(' + decl + ')')
-					//this.element.css('background', '-webkit-linear-gradient(to ' + decl + ')')
+					this.element.css('background', '-webkit-linear-gradient(' + decl + ')')
 					this.element.css('background', '-ms-linear-gradient(' + decl + ')')
 				} else {
 					this.element.css('background', '')

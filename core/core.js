fba0e0a79b189f82d0df21bd6b1158e1f2d05872
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2015-02-24 12:20:23 +0300

    added stack trace

--- core/core.js
+++ core/core.js
@@ -142,7 +142,7 @@ _globals.core.Object.prototype.onPressed = function (name, callback) {
 _globals.core.Object.prototype._update = function(name, value) {
 	if (name in this._changedHandlers) {
 		var handlers = this._changedHandlers[name];
-		handlers.forEach(function(callback) { try { callback(value)} catch(ex) { console.log("on " + name + " changed callback failed: ", ex) }})
+		handlers.forEach(function(callback) { try { callback(value)} catch(ex) { console.log("on " + name + " changed callback failed: ", ex, ex.stack) }})
 	}
 }
 
@@ -158,7 +158,7 @@ _globals.core.Object.prototype._emitSignal = function(name) {
 	args.shift();
 	if (name in this._signalHandlers) {
 		var handlers = this._signalHandlers[name];
-		handlers.forEach(function(callback) { try { callback.apply(this, args) } catch(ex) { console.log("signal " + name + " handler failed:" + ex) } });
+		handlers.forEach(function(callback) { try { callback.apply(this, args) } catch(ex) { console.log("signal " + name + " handler failed:", ex, ex.stack) } });
 	}
 }
 
@@ -411,7 +411,7 @@ exports._setup = function() {
 						if (callback(key, event))
 							return true;
 					} catch(ex) {
-						console.log("on " + key + " handler failed:", ex)
+						console.log("on " + key + " handler failed:", ex, ex.stack)
 					}
 				}
 			}
@@ -424,7 +424,7 @@ exports._setup = function() {
 						if (callback(key, event))
 							return true
 					} catch(ex) {
-						console.log("onKeyPressed handler failed:", ex)
+						console.log("onKeyPressed handler failed:", ex, ex.stack)
 					}
 				}
 			}

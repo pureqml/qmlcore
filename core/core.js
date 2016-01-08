cc279f5bd93cf9372ae217991ffd3fccdb53a806
Alexey Pobery <poberyaa@gmail.com>
2016-01-08 18:19:59 +0300

    Image is not img anymore (fix annoying image borders).

--- core/core.js
+++ core/core.js
@@ -1698,7 +1698,7 @@ exports._bootstrap = function(self, name) {
 			break;
 		case 'core.Image':
 			self.element.remove();
-			self.element = $('<img/>');
+			self.element = $('<div/>');
 			self.parent.element.append(self.element);
 			self.element.on('load', self._onLoad.bind(self));
 			self.element.on('error', self._onError.bind(self));

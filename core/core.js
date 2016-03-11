3030febffd626b17a6eaed22b31a5df51c8ecac9
Alexey Pobery <poberyaa@gmail.com>
2016-03-11 12:13:13 +0300

    Move browser traits code to 'System' prototype.

--- core/core.js
+++ core/core.js
@@ -13,21 +13,6 @@ _globals.core.webkit = navigator.userAgent.toLowerCase().indexOf('webkit') >= 0
 
 _globals.trace = { key: false, focus: false }
 
-if (navigator.userAgent.indexOf('Chromium') >= 0)
-	_globals.core.browser = "Chromium"
-else if (navigator.userAgent.indexOf('Chrome') >= 0)
-	_globals.core.browser = "Chrome"
-else if (navigator.userAgent.indexOf('Opera') >= 0)
-	_globals.core.browser = "Opera"
-else if (navigator.userAgent.indexOf('Firefox') >= 0)
-	_globals.core.browser = "Firefox"
-else if (navigator.userAgent.indexOf('Safari') >= 0)
-	_globals.core.browser = "Safari"
-else if (navigator.userAgent.indexOf('MSIE') >= 0)
-	_globals.core.browser = "IE"
-else if (navigator.userAgent.indexOf('YaBrowser') >= 0)
-	_globals.core.browser = "Yandex"
-
 if ('Common' in window) {
 	alert("[QML] samsung smart tv")
 	_globals.core.vendor = "samsung"

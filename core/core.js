6c38f565c60a0184177374fefef859a67acdff17
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-03-17 11:27:03 +0000

    added .arg() replacing

--- core/core.js
+++ core/core.js
@@ -1679,7 +1679,8 @@ exports._setup = function() {
 	}
 
 	_globals.core.core.Context.prototype.qsTr = function(text) {
-		return text
+		var args = arguments
+		return text.replace(/%(\d+)/, function(text, index) { return args[index] })
 	}
 }
 

f6d55d70d900e9bafe0ee1756ceb2c1109522b86
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-08-04 17:56:55 +0100

    moved style/updateStyle to html.Element

--- core/html.js
+++ core/html.js
@@ -4,6 +4,7 @@
 
 exports.Element = function(dom) {
 	this.dom = dom
+	this._styles = {}
 }
 
 exports.Element.prototype = Object.create(_globals.core.EventEmitter)
@@ -30,4 +31,78 @@ exports.Element.prototype.setHtml = function(html) {
 		fragment.appendChild(temp.firstChild)
 	}
 	dom.appendChild(fragment)
+}
+
+exports.Element.prototype.width = function() {
+	return this.dom.offsetWidth
+}
+
+exports.Element.prototype.height = function() {
+	return this.dom.offsetHeight
+}
+
+exports.Element.prototype.style = function(name, style) {
+	if (style !== undefined) {
+		if (style !== '') //fixme: replace it with explicit 'undefined' syntax
+			this._styles[name] = style
+		else
+			delete this._styles[name]
+		this.updateStyle()
+	} else if (name instanceof Object) { //style({ }) assignment
+		for(var k in name) {
+			var value = name[k]
+			if (value !== '') //fixme: replace it with explicit 'undefined' syntax
+				this._styles[k] = value
+			else
+				delete this._styles[k]
+		}
+		this.updateStyle()
+	}
+	else
+		return this._styles[name]
+}
+
+
+exports.Element.prototype.updateStyle = function() {
+	var element = this.element
+	if (!element)
+		return
+
+	/** @const */
+	var cssUnits = {
+		'left': 'px',
+		'top': 'px',
+		'width': 'px',
+		'height': 'px',
+
+		'border-radius': 'px',
+		'border-width': 'px',
+
+		'margin-left': 'px',
+		'margin-top': 'px',
+		'margin-right': 'px',
+		'margin-bottom': 'px'
+	}
+
+	var rules = []
+	for(var name in this._styles) {
+		var value = this._styles[name]
+		var rule = []
+
+		var prefixedName = this.getContext().getPrefixedName(name)
+		rule.push(prefixedName !== false? prefixedName: name)
+		if (Array.isArray(value))
+			value = value.join(',')
+
+		var unit = (typeof value === 'number')? cssUnits[name] || '': ''
+		value += unit
+
+		var prefixedValue = window.Modernizr.prefixedCSSValue(name, value)
+		rule.push(prefixedValue !== false? prefixedValue: value)
+
+		rules.push(rule.join(':'))
+	}
+
+	var dom = element[0]
+	dom.setAttribute('style', rules.join(';'))
 }

a418688d456d2341ac4eb02352b84fc52cd2cb0a
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-08-04 17:40:04 +0100

    moved setHtml into html.Element, route element creation to it

--- core/html.js
+++ core/html.js
@@ -9,7 +9,25 @@ exports.Element = function(dom) {
 exports.Element.prototype = Object.create(_globals.core.EventEmitter)
 exports.Element.prototype.constructor = exports.Element
 
-exports.Element.prototype.append = function(child) {
-	//add element
+exports.Element.prototype.addClass = function(cls) {
+	this.dom.classList.add(cls)
 }
 
+exports.Element.prototype.setHtml = function(html) {
+	var dom = this.dom
+	this._fragment.forEach(function(node) { dom.removeChild(node) })
+	this._fragment = []
+
+	if (html === '')
+		return
+
+	var fragment = document.createDocumentFragment()
+	var temp = document.createElement('div')
+
+	temp.innerHTML = html
+	while (temp.firstChild) {
+		this._fragment.push(temp.firstChild)
+		fragment.appendChild(temp.firstChild)
+	}
+	dom.appendChild(fragment)
+}

d1ec772c204046e37d54f4c9096952e6c4205e41
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-11 12:32:13 +0100

    fixed missing namespace

--- core/core.js
+++ core/core.js
@@ -303,7 +303,7 @@ exports.addSignal = function(self, name) {
 	})
 }
 
-exports.createSignalForwarder = function(object, name) {
+exports.core.createSignalForwarder = function(object, name) {
 	return (function() { object.emit.apply(object, copyArguments(arguments, 0, name)) })
 }
 

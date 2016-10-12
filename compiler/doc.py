d29a7ac59da5660051feb541cb8f566c54462678
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-10-12 22:54:41 +0100

    generate properties section

--- compiler/doc.py
+++ compiler/doc.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import compiler.lang as lang
 
 class Component(object):
 	def __init__(self, package, name, component):
@@ -7,10 +8,45 @@ class Component(object):
 		self.name = name
 		self.component = component
 
+	def generate_section(self, r, type, title, data):
+		if type not in data:
+			return
+
+		values = data[type]
+		r.append('## %s' %title)
+		r.append('')
+		for value in values:
+			r.append('### %s' %value.name)
+			if value.doc is not None:
+				r.append(value.doc.text)
+				r.append('')
+
+		r.append('')
+
+	def process_children(self, r):
+		component = self.component
+
+		children = {}
+
+		for child in component.children:
+			category = child.__class__.__name__
+			values = children.setdefault(category, [])
+			values.append(child)
+
+		self.generate_section(r, 'Property', 'Properties', children)
+		return r
+
+	def document(self, r, component):
+		print component.name, component.doc
+		if component.doc:
+			r.append(component.doc)
+
 	def generate(self, documentation):
 		r = []
 		package, name = self.package, self.name
-		r.append('#%s.%s' %(package, name))
+		r.append('# %s.%s Component' %(package, name))
+		r.append('')
+		self.process_children(r)
 		return '\n'.join(r)
 
 class Documentation(object):

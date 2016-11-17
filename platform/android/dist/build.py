32f584ffedeb3303091cc5329d9f9ed38faec1a8
comrat <poberyaa@gmail.com>
2016-11-18 00:37:29 +0300

    Remove generated files add generate script instead.

---
+++ platform/android/dist/build.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+import argparse
+import os
+
+
+def build(app, title):
+    os.system('rm -rf %s' %app)
+    res = os.system('cordova create %s com.example.app %s' %(app,title))
+    if res != 0:
+    	print "Failed to create android app"
+        return
+    os.chdir(app)
+    os.system('cordova platform add android')
+    os.system('cordova build android')
+    os.chdir('..')
+    os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
+
+
+parser = argparse.ArgumentParser('qmlcore build tool')
+parser.add_argument('--app', '-a', help='application name', default="app")
+parser.add_argument('--title', '-t', help='application title', default="App")
+args = parser.parse_args()
+
+build(args.app, args.title)

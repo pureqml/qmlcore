e4048c57c169c6307d6763d4cc97a2a0f43609f8
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-11-22 23:48:57 +0000

    added context initialization

--- compiler/js/generator.py
+++ compiler/js/generator.py
@@ -193,6 +193,8 @@ class generator(object):
 		startup = []
 		startup.append('\tvar l10n = %s\n' %json.dumps(self.l10n))
 		startup.append("\t%s._context = new qml.core.Context()" %ns)
+		startup.append('\t%s._context.__create()' %ns)
+		startup.append('\t%s._context.__setup()' %ns)
 		startup.append("\t%s._context.init({id: 'qml-context-%s', prefix: '%s', l10n: l10n})" %(ns, app, prefix))
 		startup += self.startup
 		r += "\n".join(startup)

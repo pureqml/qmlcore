ae2cac66ed58d31a9e0dd49186251dd8485b712a
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-12-11 20:42:59 +0000

    added sequental insert test

--- test/update.js
+++ test/update.js
@@ -15,4 +15,17 @@ describe('ModelUpdate', function() {
 			model.apply(view)
 		})
 	})
+
+	describe('sequental insert', function() {
+		it('should set single insert range', function() {
+			model = new Model()
+			view = new View()
+			mock = sinon.mock(view)
+			mock.expects('_insertItems').once().withArgs(0, 10)
+			for(var i = 0; i < 10; ++i)
+				model.insert(0, 1)
+			view.length(10)
+			model.apply(view)
+		})
+	})
 })

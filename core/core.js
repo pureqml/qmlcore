8009e7094627c6d4e6d5c84ce6539ee37de74549
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-01-16 22:34:26 +0000

    moved os/device checks to html5/.core.js

--- core/core.js
+++ core/core.js
@@ -1,6 +1,5 @@
 //WARNING: no log() function usage before init.js
 
-exports.core.os = navigator.platform
 exports.core.device = 0
 exports.core.vendor = ""
 
@@ -49,26 +48,6 @@ if (log === null)
 /** @const */
 /** @param {string} text @param {...} args */
 _globals.qsTr = function(text, args) { return _globals._context.qsTr.apply(qml._context, arguments) }
-
-
-var _checkDevice = function(target, info) {
-	if (navigator.userAgent.indexOf(target) < 0)
-		return
-
-	log("[QML] " + target)
-	exports.core.vendor = info.vendor
-	exports.core.device = info.device
-	exports.core.os = info.os
-	log("loaded")
-}
-
-if (!exports.core.vendor) {
-	_checkDevice('Blackberry', { 'vendor': 'blackberry', 'device': 2, 'os': 'blackberry' })
-	_checkDevice('Android', { 'vendor': 'google', 'device': 2, 'os': 'android' })
-	_checkDevice('iPhone', { 'vendor': 'apple', 'device': 2, 'os': 'iOS' })
-	_checkDevice('iPad', { 'vendor': 'apple', 'device': 2, 'os': 'iOS' })
-	_checkDevice('iPod', { 'vendor': 'apple', 'device': 2, 'os': 'iOS' })
-}
 
 var colorTable = {
 	'maroon':	'800000',

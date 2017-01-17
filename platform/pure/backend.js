8ad77b9e3d97d673d5e9562399d4f90a881bbf17
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-01-17 21:49:46 +0000

    added Backend and Element classes stubs

--- platform/pure/backend.js
+++ platform/pure/backend.js
@@ -1,1 +1,16 @@
 exports.capabilities = {}
+
+var Element = function(tag) {
+
+}
+
+Element.prototype.constructor = Element
+
+var Backend = function() {
+
+}
+
+Backend.prototype.constructor = Backend
+
+exports.Backend = Backend
+exports.Element = Element

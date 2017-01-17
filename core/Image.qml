92c13180797cf3100d9ccda8b51218d308c78770
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-01-17 23:27:34 +0000

    removed Backend class, moved html5 image specific code into html5 backend

--- core/Image.qml
+++ core/Image.qml
@@ -13,87 +13,7 @@ Item {
 			self._load()
 		})
 
-		this._init()
-	}
-
-	function _init() {
-		var tmp = new Image()
-		this._image = tmp
-		this._image.onerror = this._onError.bind(this)
-
-		var image = this
-		this._image.onload = function() {
-			image.paintedWidth = tmp.naturalWidth
-			image.paintedHeight = tmp.naturalHeight
-
-			var style = {'background-image': 'url(' + image.source + ')'}
-			switch(image.fillMode) {
-				case image.Stretch:
-					style['background-repeat'] = 'no-repeat'
-					style['background-size'] = '100% 100%'
-					break;
-				case image.TileVertically:
-					style['background-repeat'] = 'repeat-y'
-					style['background-size'] = '100%'
-					break;
-				case image.TileHorizontally:
-					style['background-repeat'] = 'repeat-x'
-					style['background-size'] = tmp.naturalWidth + 'px 100%'
-					break;
-				case image.PreserveAspectFit:
-					style['background-repeat'] = 'no-repeat'
-					style['background-position'] = 'center'
-					var w = image.width
-					var h = image.height
-					var wPart = w / tmp.naturalWidth
-					var hPart = h / tmp.naturalHeight
-					var wRatio = 100
-					var hRatio = 100
-
-					if (wPart === 0) {
-						wPart = hPart
-						w = hPart * tmp.naturalWidth
-					}
-
-					if (hPart === 0) {
-						hPart = wPart
-						h = wPart * tmp.naturalHeight
-					}
-
-					if (wPart > hPart)
-						wRatio = Math.floor(100 / wPart * hPart)
-					else
-						hRatio = Math.floor(100 / hPart * wPart)
-					style['background-size'] = wRatio + '% ' + hRatio + '%'
-					image.paintedWidth = w * wRatio / 100
-					image.paintedHeight = h * hRatio / 100
-					break;
-				case image.PreserveAspectCrop:
-					style['background-repeat'] = 'no-repeat'
-					style['background-position'] = 'center'
-					var pRatio = tmp.naturalWidth / tmp.naturalHeight
-					var iRatio = image.width / image.height
-					if (pRatio < iRatio) {
-						var hRatio = Math.floor(iRatio / pRatio * 100)
-						style['background-size'] = 100 + '% ' + hRatio + '%'
-					}
-					else {
-						var wRatio = Math.floor(pRatio / iRatio * 100)
-						style['background-size'] = wRatio + '% ' + 100 + '%'
-					}
-					break;
-				case image.Tile:
-					style['background-repeat'] = 'repeat-y repeat-x'
-					break;
-			}
-			image.style(style)
-
-			if (!image.width)
-				image.width = image.paintedWidth
-			if (!image.height)
-				image.height = image.paintedHeight
-			image.status = Image.Ready
-		}
+		this._context.backend.initImage(this)
 		this.load()
 	}
 
@@ -102,12 +22,11 @@ Item {
 	}
 
 	function _load() {
-		this._image.src = this.source
+		this._context.backend.loadImage(this)
 	}
 
 	function load() {
-		var src = this.source
-		this.status = (src.length === 0)? Image.Null: Image.Loading
+		this.status = (this.source.length === 0)? Image.Null: Image.Loading
 		this._delayedLoad.schedule()
 	}
 
--- platform/html5/html.js
+++ platform/html5/html.js
@@ -314,7 +314,7 @@ exports.getElement = function(tag) {
 	return new exports.Element(this, tags[0])
 }
 
-exports.Backend = function(ctx) {
+exports.init = function(ctx) {
 	var options = ctx.options
 	var prefix = ctx._prefix
 
@@ -381,7 +381,93 @@ exports.Backend = function(ctx) {
 	ctx.screenHeight = window.screen.height
 }
 
-exports.Backend.prototype.constructor = exports.Backend
+exports.createElement = function(ctx, tag) {
+	return new exports.Element(ctx, tag)
+}
+
+exports.initImage = function(image) {
+	var tmp = new Image()
+	image._image = tmp
+	image._image.onerror = image._onError.bind(image)
+
+	image._image.onload = function() {
+		image.paintedWidth = tmp.naturalWidth
+		image.paintedHeight = tmp.naturalHeight
+
+		var style = {'background-image': 'url(' + image.source + ')'}
+		switch(image.fillMode) {
+			case image.Stretch:
+				style['background-repeat'] = 'no-repeat'
+				style['background-size'] = '100% 100%'
+				break;
+			case image.TileVertically:
+				style['background-repeat'] = 'repeat-y'
+				style['background-size'] = '100%'
+				break;
+			case image.TileHorizontally:
+				style['background-repeat'] = 'repeat-x'
+				style['background-size'] = tmp.naturalWidth + 'px 100%'
+				break;
+			case image.PreserveAspectFit:
+				style['background-repeat'] = 'no-repeat'
+				style['background-position'] = 'center'
+				var w = image.width
+				var h = image.height
+				var wPart = w / tmp.naturalWidth
+				var hPart = h / tmp.naturalHeight
+				var wRatio = 100
+				var hRatio = 100
+
+				if (wPart === 0) {
+					wPart = hPart
+					w = hPart * tmp.naturalWidth
+				}
+
+				if (hPart === 0) {
+					hPart = wPart
+					h = wPart * tmp.naturalHeight
+				}
+
+				if (wPart > hPart)
+					wRatio = Math.floor(100 / wPart * hPart)
+				else
+					hRatio = Math.floor(100 / hPart * wPart)
+				style['background-size'] = wRatio + '% ' + hRatio + '%'
+				image.paintedWidth = w * wRatio / 100
+				image.paintedHeight = h * hRatio / 100
+				break;
+			case image.PreserveAspectCrop:
+				style['background-repeat'] = 'no-repeat'
+				style['background-position'] = 'center'
+				var pRatio = tmp.naturalWidth / tmp.naturalHeight
+				var iRatio = image.width / image.height
+				if (pRatio < iRatio) {
+					var hRatio = Math.floor(iRatio / pRatio * 100)
+					style['background-size'] = 100 + '% ' + hRatio + '%'
+				}
+				else {
+					var wRatio = Math.floor(pRatio / iRatio * 100)
+					style['background-size'] = wRatio + '% ' + 100 + '%'
+				}
+				break;
+			case image.Tile:
+				style['background-repeat'] = 'repeat-y repeat-x'
+				break;
+		}
+		image.style(style)
+
+		if (!image.width)
+			image.width = image.paintedWidth
+		if (!image.height)
+			image.height = image.paintedHeight
+		image.status = Image.Ready
+	}
+
+}
+
+exports.loadImage = function(image) {
+	image._image.src = image.source
+}
 
 var Modernizr = window.Modernizr
 
@@ -397,7 +483,3 @@ exports.cancelAnimationFrame = Modernizr.prefixed('cancelAnimationFrame', window
 exports.enterFullscreenMode = function(el) { return Modernizr.prefixed('requestFullscreen', el.dom)() }
 exports.exitFullscreenMode = function() { return window.Modernizr.prefixed('exitFullscreen', document)() }
 exports.inFullscreenMode = function () { return !!window.Modernizr.prefixed('fullscreenElement', document) }
-
-exports.createElement = function(ctx, tag) {
-	return new exports.Element(ctx, tag)
-}

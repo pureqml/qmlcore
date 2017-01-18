427c0a3d49412a52b35b5c687b9eb3cc08d4fbf8
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-01-18 21:53:15 +0000

    log key operation in pure backend

--- platform/pure/backend.js
+++ platform/pure/backend.js
@@ -8,7 +8,9 @@ Element.prototype.constructor = Element
 Element.prototype.append = function(child) { }
 Element.prototype.addClass = function(cls) { }
 Element.prototype.setHtml = function(cls) { }
-Element.prototype.style = function(name, value) { }
+Element.prototype.style = function(name, value) {
+	log('style', name, value)
+}
 
 exports.init = function(ctx) {
 	ctx.element = new Element(ctx, ctx.getTag())
@@ -19,13 +21,12 @@ exports.createElement = function(ctx, tag) {
 }
 
 exports.initImage = function(image) {
-
 }
 
 exports.loadImage = function(image) {
-
+	log('loading image from ' + image.source)
 }
 
 exports.layoutText = function(text) {
-
+	log('laying out text ' + text)
 }

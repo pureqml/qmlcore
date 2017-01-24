6fdc7fd97200e424dcd8c8642bf586fc2b97d7ae
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-01-24 20:45:11 +0000

    added RAIIEventListener for pure, implemented children appending

--- platform/pure/backend.js
+++ platform/pure/backend.js
@@ -1,11 +1,43 @@
+/*** @used { core.RAIIEventEmitter } **/
+
 exports.capabilities = {}
 
-var Element = function(context, tag) {
-	this._context = context
+var registerGenericListener = function(target) {
+	var prefix = '__nativeEventHandler_'
+	target.onListener('',
+		function(name) {
+			log('registering generic event', name)
+			var pname = prefix + name
+			var callback = target[pname] = function() {
+				COPY_ARGS(args, 0)
+				target.emitWithArgs(name, args)
+			}
+			//target.dom.addEventListener(name, callback)
+		},
+		function(name) {
+			log('removing generic event', name)
+			var pname = prefix + name
+			//target.dom.removeEventListener(name, target[pname])
+		}
+	)
 }
 
+
+var Element = function(context, tag) {
+	_globals.core.RAIIEventEmitter.apply(this)
+	this._context = context
+	this.children = []
+	registerGenericListener(this)
+}
+
+Element.prototype = Object.create(_globals.core.RAIIEventEmitter.prototype)
+Element.prototype.constructor = exports.Element
+
 Element.prototype.constructor = Element
-Element.prototype.append = function(child) { }
+Element.prototype.append = function(child) {
+	this.children.push(child)
+}
+
 Element.prototype.addClass = function(cls) { }
 Element.prototype.setHtml = function(cls) { }
 Element.prototype.style = function(name, value) {

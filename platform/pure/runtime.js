0e2408b2640a26241cb18ecd1eaa5c9bbfe96edf
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-04-21 22:33:07 +0100

    moved Element to pure.runtime

--- platform/pure/runtime.js
+++ platform/pure/runtime.js
@@ -1,3 +1,5 @@
+/*** @using { core.RAIIEventEmitter } **/
+
 var Rect = function(l, t, r, b) {
 	this.l = l || 0
 	this.t = t || 0
@@ -98,5 +100,118 @@ Renderer.prototype.paintImage = function(rect, image) {
 	_renderImage(rect.l, rect.t, rect.r, rect.b, image)
 }
 
+var Element = function(context, tag, paint) {
+	_globals.core.RAIIEventEmitter.apply(this)
+	this._context = context
+	this._styles = {}
+	this._paint = paint
+	this.children = []
+	registerGenericListener(this)
+}
+
+Element.prototype = Object.create(_globals.core.RAIIEventEmitter.prototype)
+Element.prototype.constructor = Element
+
+Element.prototype.addClass = function(cls) { }
+
+var importantStyles = new Set([
+	'left', 'top', 'width', 'height', 'visibility',
+	'background-color', 'border-radius',
+	'color', 'font-size', 'text-align'
+])
+
+Element.prototype._onUpdate = function(name) {
+	if (importantStyles.has(name))
+		this.update()
+//	else
+//		log('unhandled style ' + name)
+}
+
+Element.prototype.style = function(name, style) {
+	if (style !== undefined) {
+		this._onUpdate(name)
+		if (style !== '') //fixme: replace it with explicit 'undefined' syntax
+			this._styles[name] = style
+		else
+			delete this._styles[name]
+	} else if (name instanceof Object) { //style({ }) assignment
+		for(var k in name) {
+			this._onUpdate(k)
+			var value = name[k]
+			if (value !== '') //fixme: replace it with explicit 'undefined' syntax
+				this._styles[k] = value
+			else
+				delete this._styles[k]
+		}
+	}
+	else
+		return this._styles[name]
+}
+Element.prototype.updateStyle = function() { }
+
+Element.prototype.visible = function() {
+	var visibility = this._styles['visibility']
+	return visibility !== 'hidden'
+}
+
+Element.prototype.update = function() {
+	if (!this.visible())
+		return
+	updatedItems.add(this)
+	if (updateTimer === undefined) {
+		updateTimer = setTimeout(function() {
+			log('frame paint')
+			updateTimer = undefined
+			updatedItems.clear()
+			rootItem.paint(renderer, 0, 0)
+		}, 0)
+	}
+}
+
+Element.prototype.append = function(child) {
+	if (child._parent !== undefined)
+		throw new Error('double append on element')
+	child._parent = this
+	this.children.push(child)
+	child.update()
+}
+
+Element.prototype.remove = function() {
+	var parent = this._parent
+	if (parent !== undefined) {
+		var idx = parent.children.indexOf(this)
+		if (idx < 0)
+			throw new Error('remove(): no child in parent children array')
+		parent.children.splice(idx, 1)
+		parent.update()
+		this._parent = undefined
+	} else
+		throw new Error('remove() called without adding to parent')
+}
+
+Element.prototype.getRect = function() {
+	var style = this._styles
+	var l = style['left'] || 0, t = style['top'] || 0
+	var w = style['width'] || 0, h = style['height'] || 0
+	return new Rect(l, t, w + l, h + t)
+}
+
+Element.prototype.paint = function(renderer, x, y) {
+	var visibility = this._styles['visibility']
+	if (visibility === 'hidden')
+		return
+
+	var rect = this.getRect()
+	rect.move(x, y)
+	this._paint(renderer, rect)
+	++renderer.depth
+	this.children.forEach(function(child) {
+		child.paint(renderer, rect.l, rect.t)
+	})
+	--renderer.depth
+}
+
+
 exports.Rect = Rect
 exports.Renderer = Renderer
+exports.Element = Element

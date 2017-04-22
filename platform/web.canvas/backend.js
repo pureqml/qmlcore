d85f3113c54e878aaaa3383e8e0360e9fd7ac59f
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-04-22 13:13:27 +0100

    added stubs for text and fixed item update/schedule redraw on start

--- platform/web.canvas/backend.js
+++ platform/web.canvas/backend.js
@@ -8,12 +8,16 @@ var canvas = null
 var proxy = [
 	'requestAnimationFrame', 'cancelAnimationFrame',
 	'enterFullscreenMode', 'exitFullscreenMode', 'inFullscreenMode',
-//	'initImage', 'loadImage', 'initText', 'layoutText',  //this should not be proxy, implement this backend methods
-	'run'
+	'initImage', 'loadImage',
+	//'initText', 'layoutText', //this should not be proxy, implement this backend methods
 ]
 
 var paintRect = function(renderer, rect) {
 	console.log('paintRect: ' + rect)
+}
+
+var paintText = function(renderer, rect) {
+	console.log('paintText: ' + rect)
 }
 
 exports.init = function(ctx) {
@@ -29,7 +33,8 @@ exports.init = function(ctx) {
 	html.init(ctx)
 	canvas = ctx.element.dom
 	ctx._updatedItems = new Set()
-	ctx.element = new runtime.Element(ctx, ctx.getTag())
+	ctx.element = exports.createElement(ctx, ctx.getTag())
+	ctx.renderer = new runtime.Renderer(ctx.width, ctx.height)
 	{
 		var Element = runtime.Element
 		Element.prototype.setHtml = function(html) {
@@ -43,9 +48,26 @@ exports.init = function(ctx) {
 	}
 }
 
+exports.run = function(ctx) {
+	console.log('calling redraw')
+	ctx.element.update()
+}
+
 exports.createElement = function(ctx, tag) {
 	if (runtime === null)
 		runtime = _globals.pure.runtime //fixme: this is called from StyleSheet too early (ctor?), fix initialisation order!
 	return new runtime.Element(ctx, tag, paintRect)
 }
 
+exports.initText = function(text) {
+	var element = text.element
+	element._offsetX = 0
+	element._offsetY = 0
+	element.ui = text
+	element._paint = paintText
+	element.update()
+}
+
+exports.layoutText = function(text) {
+	console.log('layoutText stub')
+}

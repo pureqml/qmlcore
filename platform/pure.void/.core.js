a962bd83dcda5f129bc3dbb32011a1df134b9adb
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-05-15 13:33:29 +0100

    moved stubs for in pure/ created new pure.void platform

---
+++ platform/pure.void/.core.js
@@ -0,0 +1,1 @@
+_globals._backend = function() { return _globals.pure.void.backend }
---
+++ platform/pure/.core.js
@@ -0,0 +1,3 @@
+_globals.core.os = 'linux'
+_globals.core.userAgent = 'pure-native'
+_globals.core.language = 'en'

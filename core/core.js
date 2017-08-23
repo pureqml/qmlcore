a5966cba576c1afc4ed1e7947a5756b8764d14ba
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-08-23 19:48:21 +0100

    added bind implementation for older webkits, replaced console.log with less efficient function

--- core/core.js
+++ core/core.js
@@ -7,6 +7,40 @@ exports.core.trace = { key: false, focus: false, listeners: false }
 
 /* ${init.js} */
 
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function(oThis) {
+		if (typeof this !== 'function') {
+			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable')
+		}
+
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
+			fNOP    = function() {},
+			fBound  = function() {
+				return fToBind.apply(this instanceof fNOP && oThis
+					? this
+					: oThis,
+					aArgs.concat(Array.prototype.slice.call(arguments)))
+			}
+
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+
+			return fBound;
+	}
+
+	if (log === null) {
+		//old webkits with no bind don't allow binding console.log
+		log = function() {
+			var line = ''
+			for(var i = 0; i < arguments.length; ++i) {
+				line += arguments[i] + ' '
+			}
+			console.log(line)
+		}
+	}
+}
+
 if (log === null)
 	log = console.log.bind(console)
 

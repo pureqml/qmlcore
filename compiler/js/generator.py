a2857bd9c86521f1471d1ab7ca2d320448daf0d2
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-08-30 10:32:58 +0100

    Revert "collect ids in pregenerate phase, removed id_set rewrite hack"
    This reverts commit 0120132c7570c0f097d59ec47d51e99d2c052fcc.

--- compiler/js/generator.py
+++ compiler/js/generator.py
@@ -15,7 +15,6 @@ class generator(object):
 		self.packages = {}
 		self.startup = []
 		self.l10n = {}
-		self.id_set = set(['context'])
 
 	def add_component(self, name, component, declaration):
 		if name in self.components:
@@ -88,6 +87,8 @@ class generator(object):
 	def generate_component(self, gen):
 		name = gen.name
 
+		self.id_set = set(['context'])
+		gen.collect_id(self.id_set)
 		self.used_packages.add(gen.package)
 
 		code = ''
@@ -126,7 +127,6 @@ class generator(object):
 		code, base_class = {}, {}
 
 		for gen in self.components.itervalues():
-			gen.collect_id(self.id_set)
 			gen.pregenerate(self)
 
 		while queue or self.used_components:

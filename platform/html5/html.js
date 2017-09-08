d9e09dcc8b549f029473993fad54e82524f3deb2
Vadim Kravchenko <vadim.kravchenko@gmail.com>
2017-09-08 03:34:52 +0300

    display replaced back with visibility

--- platform/html5/html.js
+++ platform/html5/html.js
@@ -42,8 +42,6 @@ StyleCachePrototype.update = function(element, name) {
 			data[name] = true
 		cache[id] = entry
 	}
-	if (name === 'display')
-		element.updateStyle()
 }
 
 StyleCachePrototype.pop = function(element) {
@@ -758,7 +756,7 @@ var cssMappings = {
 	rotate: 'transform',
 	boxshadow: 'box-shadow',
 	transform: 'transform',
-	visible: 'display', visibleInView: 'display',
+	visible: 'visibility', visibleInView: 'visibility',
 	background: 'background',
 	color: 'color',
 	backgroundImage: 'background-image',

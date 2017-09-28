d4bdd5b6837b6514f6adee3980e2127bfeaaf665
Pobery Alexey <poberyaa@gmail.com>
2017-09-29 01:28:35 +0300

    Introducce cordova based ios platform

---
+++ platform/ios/dist/build.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+import argparse
+import os
+
+
+def build(app, title):
+    os.system('rm -rf %s' %app)
+    res = os.system('cordova create %s com.example.app %s' %(app,title))
+    if res != 0:
+        print "Failed to create ios app"
+        return
+    os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
+    os.system('cp androidIcon.png %s' %(app))
+    os.system('cp config.xml %s' %(app))
+    os.chdir(app)
+    os.system('cordova platform add ios')
+    os.system('cordova build ios')
+    os.chdir('..')
+
+
+parser = argparse.ArgumentParser('qmlcore build tool')
+parser.add_argument('--app', '-a', help='application name', default="app")
+parser.add_argument('--title', '-t', help='application title', default="App")
+args = parser.parse_args()
+
+
+res = os.system('cordova --version')
+if res == 0:
+    build(args.app, args.title)
+else:
+    print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'

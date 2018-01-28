db1072bc909cc419b595bab98b021fb67d9afda7
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-01-28 22:26:27 +0000

    added key codes for android

--- platform/pure.femto/.core.js
+++ platform/pure.femto/.core.js
@@ -1,2 +1,31 @@
 _globals._backend = function() { return _globals.pure.femto.backend }
 _globals.core.__locationBackend = function() { return _globals.pure.femto.location }
+
+//android NDK keycodes
+exports.core.keyCodes = {
+	4:	'Back',
+
+	7:	'0',
+	8:	'1',
+	9:	'2',
+	10:	'3',
+	11:	'4',
+	12:	'5',
+	13:	'6',
+	14:	'7',
+	15:	'8',
+	16:	'9',
+
+	19:	'Up',
+	20:	'Down',
+	21:	'Left',
+	22:	'Right',
+	23:	'Select',
+
+	62:	'Space',
+
+	183:	'Red',
+	184:	'Green',
+	185:	'Yellow',
+	186:	'Blue'
+}

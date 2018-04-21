19ee6dd071e4e7c708d41b4f3fda1631081f79c6
Alexey Pobery <poberyaa@gmail.com>
2018-04-21 21:02:32 +0300

    Implement release apk building for android

--- platform/android/dist/build.py
+++ platform/android/dist/build.py
@@ -4,37 +4,46 @@ import argparse
 import os
 
 
-def build(app, title):
-    os.system('rm -rf %s' %app)
-    res = os.system('cordova create %s com.%s.app %s' %(app, app, title))
-    if res != 0:
-        print "Failed to create android app"
-        return
-    os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
-    os.system('cp androidIcon.png %s' %(app))
-    os.system('cp config.xml %s' %(app))
-    os.chdir(app)
+def build(app, title, release):
+	os.system('rm -rf %s' %app)
+	res = os.system('cordova create %s com.%s.app %s' %(app, app, title))
+	if res != 0:
+		print "Failed to create android app"
+		return
+	os.system('cp -r `ls -A | grep -v "%s"` %s/www' %(app,app))
+	os.system('cp androidIcon.png %s' %(app))
+	os.system('cp config.xml %s' %(app))
+	os.chdir(app)
 
-    {% block commands %}{% endblock %}
 
-    os.system('cordova platform add android')
-    os.system('cordova plugin add cordova-plugin-streaming-media')
-    os.system('cordova plugin add cordova-plugin-device')
+	os.system('cordova platform add android')
+	{% block commands %}{% endblock %}
+	os.system('cordova plugin add cordova-plugin-streaming-media')
+	os.system('cordova plugin add cordova-plugin-device')
+	{% block plugins %}{% endblock %}
 
-    {% block plugins %}{% endblock %}
+	if release:
+		{% if androidBuild %}
+		build = 'cordova build android --release -- '
+		os.system(build + '--keystore={{androidBuild.keystore}} --storePassword={{androidBuild.storePassword}} --alias={{androidBuild.alias}} --password={{androidBuild.password}}')
+		{% else %}
+		print "Failed to build release apk androidBuild property is undefined"
+		{% endif %}
+	else:
+		os.system('cordova build android')
 
-    os.system('cordova build android')
-    os.chdir('..')
+	os.chdir('..')
 
 
 parser = argparse.ArgumentParser('pureqml cordova android build tool')
 parser.add_argument('--app', '-a', help='application name', default="app")
 parser.add_argument('--title', '-t', help='application title', default="App")
+parser.add_argument('--release', '-r', help='build release apk', default=False)
 args = parser.parse_args()
 
 
 res = os.system('cordova --version')
 if res == 0:
-    build(args.app, args.title)
+	build(args.app, args.title, release)
 else:
-    print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'
+	print 'Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/'

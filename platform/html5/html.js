5711cd2f377634a1774ffa814543e1205e0c5f0a
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-03-14 11:42:00 +0000

    added log in fullscreen change event

--- platform/html5/html.js
+++ platform/html5/html.js
@@ -505,6 +505,7 @@ exports.init = function(ctx) {
 
 	var onFullscreenChanged = function(e) {
 		var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+		log('fullscreen change event:', e, document.fullScreen, document.mozFullScreen, document.webkitIsFullScreen)
 		ctx.fullscreen = state
 	}
 

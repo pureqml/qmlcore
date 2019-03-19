79a6c8a8b112be2b9768b9279d093ed93fab7379
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-03-19 09:40:50 +0000

    do not register shaka if browser is not supported

--- platform/video.shaka/.core.js
+++ platform/video.shaka/.core.js
@@ -1,1 +1,4 @@
-_globals.core.__videoBackends.shaka = function() { return _globals.video.shaka.backend }
+shaka.polyfill.installAll();
+if (shaka.Player.isBrowserSupported()) {
+    _globals.core.__videoBackends.shaka = function() { return _globals.video.shaka.backend }
+}

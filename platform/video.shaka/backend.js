a4e30078770fa292eeabdd190f9c51fa89fb3884
Alexey <poberyaa@gmail.com>
2019-04-02 16:30:43 +0300

    Issue-150: move common html5 player code to video.html5 backend (#151)
    LGTM

--- platform/video.shaka/backend.js
+++ platform/video.shaka/backend.js
@@ -23,72 +23,9 @@ var Player = function(ui) {
 	var shakaPlayer = new shaka.Player(player.dom);
 	this.shakaPlayer = shakaPlayer;
 
-	var dom = player.dom
-	player.on('play', function() { ui.waiting = false; ui.paused = dom.paused }.bind(ui))
-	player.on('pause', function() { ui.paused = dom.paused }.bind(ui))
-	player.on('ended', function() { ui.finished() }.bind(ui))
-	player.on('seeked', function() { log("seeked"); ui.seeking = false; ui.waiting = false }.bind(ui))
-	player.on('canplay', function() { log("canplay", dom.readyState); ui.ready = dom.readyState }.bind(ui))
-	player.on('seeking', function() { log("seeking"); ui.seeking = true; ui.waiting = true }.bind(ui))
-	player.on('waiting', function() { log("waiting"); ui.waiting = true }.bind(ui))
-	player.on('stalled', function() { log("Was stalled", dom.networkState); ui.waiting = true; ui.stalled = true }.bind(ui))
-	player.on('emptied', function() { log("Was emptied", dom.networkState); }.bind(ui))
-	player.on('volumechange', function() { ui.muted = dom.muted }.bind(ui))
-	player.on('canplaythrough', function() { log("ready to play"); ui.paused = dom.paused }.bind(ui))
-
-	var onError = function() {
-		log("Player error occurred", dom.error, "src", ui.source)
-
-		if (!dom.error || !ui.source)
-			return
-
-		ui.error(dom.error)
-
-		log("player.error", dom.error)
-		switch (dom.error.code) {
-		case 1:
-			log("MEDIA_ERR_ABORTED error occurred")
-			break;
-		case 2:
-			log("MEDIA_ERR_NETWORK error occurred")
-			break;
-		case 3:
-			log("MEDIA_ERR_DECODE error occurred")
-			break;
-		case 4:
-			log("MEDIA_ERR_SRC_NOT_SUPPORTED error occurred")
-			break;
-		default:
-			log("UNDEFINED error occurred")
-			break;
-		}
-	}.bind(ui)
-
-	player.on('error', onError)
-	this.shakaPlayer.addEventListener('error', ui._context.wrapNativeCallback(onError));
-
-	player.on('timeupdate', function() {
-		ui.waiting = false
-		ui.stalled = false
-		if (!ui.seeking)
-			ui.progress = dom.currentTime
-	}.bind(ui))
-
-	player.on('durationchange', function() {
-		var d = dom.duration
-		log("Duration", d)
-		ui.duration = isFinite(d) ? d : 0
-	}.bind(ui))
-
-	player.on('progress', function() {
-		var last = dom.buffered.length - 1
-		ui.waiting = false
-		if (last >= 0)
-			ui.buffered = dom.buffered.end(last) - dom.buffered.start(last)
-	}.bind(ui))
-
 	this.element = player
 	this.ui = ui
+	this.setEventListeners()
 	this.setAutoPlay(ui.autoPlay)
 
 	ui.element.remove()

6fb7c409c2b4d7efe80f4263ff585a8f94b2a3d7
Alexey Pobery <poberyaa@gmail.com>
2019-06-25 20:30:54 +0300

    Remove 'waiting' flag setting in 'stalled' handler

--- platform/video.html5/backend.js
+++ platform/video.html5/backend.js
@@ -25,7 +25,7 @@ Player.prototype.setEventListeners = function() {
 	player.on('canplay', function() { log("canplay", dom.readyState); ui.ready = dom.readyState }.bind(ui))
 	player.on('seeking', function() { log("seeking"); ui.seeking = true; ui.waiting = true }.bind(ui))
 	player.on('waiting', function() { log("waiting"); ui.waiting = true }.bind(ui))
-	player.on('stalled', function() { log("Was stalled", dom.networkState); ui.waiting = true; ui.stalled = true }.bind(ui))
+	player.on('stalled', function() { log("Was stalled", dom.networkState); ui.stalled = true }.bind(ui))
 	player.on('emptied', function() { log("Was emptied", dom.networkState); }.bind(ui))
 	player.on('volumechange', function() { ui.muted = dom.muted }.bind(ui))
 	player.on('canplaythrough', function() { log("ready to play"); ui.paused = dom.paused }.bind(ui))

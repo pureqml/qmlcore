b517ebaa6128f470cc7e1d99f8ee96aaf75a9395
Alexey Pobery <poberyaa@gmail.com>
2019-10-21 21:32:20 +0300

    Add log messages buffer while loggerpanel is not initialized

--- platform/loggerpanel/.core.js
+++ platform/loggerpanel/.core.js
@@ -1,11 +1,20 @@
 var logPanelFlag = window["$manifest$logPanel"]
+var logMessagesBuffer = []
 
 if (logPanelFlag) {
 	log = function(dummy) {
 		var args = Array.prototype.slice.call(arguments)
 		var logger = document.getElementById("logger") || undefined
-		if (logger)
+		if (logger) {
+			if (logMessagesBuffer.length > 0) {
+				for (var i = 0; i < logMessagesBuffer.length; ++i)
+					logger.innerHTML += logMessagesBuffer[i] + "<br>"
+				logMessagesBuffer = []
+			}
 			logger.innerHTML += args.join(" ") + "<br>"
+		} else {
+			logMessagesBuffer.push(args.join(" "))
+		}
 	}
 } else {
 	log = console.log.bind(console)

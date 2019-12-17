e55936abce553b56eecfd67dc2a4c5db1ac0bef3
Alexey Pobery <poberyaa@gmail.com>
2019-12-17 18:23:38 +0300

    Add cordova-plugin-ip-mac-address plugin in android build

--- platform/android/dist/build.py
+++ platform/android/dist/build.py
@@ -22,6 +22,7 @@ def build(app, title, release):
 	os.system('cordova plugin add cordova-plugin-streaming-media')
 	os.system('cordova plugin add cordova-plugin-device')
 	os.system('cordova plugin add cordova-plugin-screen-orientation')
+	os.system('cordova plugin add cordova-plugin-ip-mac-address')
 	{% block plugins %}{% endblock %}
 
 	if release:

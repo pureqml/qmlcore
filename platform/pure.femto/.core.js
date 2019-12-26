c3ee80e8f014205392816f0a52402d76d7a1bbd5
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-12-26 22:13:03 +0000

    reflect android-native change and get .on() from fd.Object

--- platform/pure.femto/.core.js
+++ platform/pure.femto/.core.js
@@ -1,6 +1,8 @@
 _globals._backend = function() { return _globals.pure.femto.backend }
+_globals.core.__deviceBackend = function() { return _globals.pure.femto.device }
 _globals.core.__locationBackend = function() { return _globals.pure.femto.location }
 _globals.core.__videoBackends.femto = function() { return _globals.pure.femto.video }
+_globals.core.__localStorageBackend = function() { return _globals.pure.femto.storage }
 
 //android NDK keycodes
 exports.core.keyCodes = {

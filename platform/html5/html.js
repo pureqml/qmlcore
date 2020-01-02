697c2e10bccd103b11062426ef6667c89d4365d9
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-01-02 18:41:15 +0000

    move ajax implementation to general backend

--- platform/html5/html.js
+++ platform/html5/html.js
@@ -869,6 +869,38 @@ exports.enterFullscreenMode = function(el) { return Modernizr.prefixed('requestF
 exports.exitFullscreenMode = function() { return window.Modernizr.prefixed('exitFullscreen', document)() }
 exports.inFullscreenMode = function () { return !!window.Modernizr.prefixed('fullscreenElement', document) }
 
+exports.ajax = function(request) {
+	var url = request.url
+	var error = request.error,
+		headers = request.headers,
+		done = request.done,
+		settings = request.settings
+
+	var xhr = new XMLHttpRequest()
+
+	var self = this
+	var ctx = this._context
+	if (error)
+		xhr.addEventListener('error', ctx.wrapNativeCallback(function(event) { self.loading = false; log("Error"); error(event); }))
+
+	if (done)
+		xhr.addEventListener('load', ctx.wrapNativeCallback(function(event) { self.loading = false; done(event); }))
+
+	xhr.open(request.method || 'GET', url);
+
+	for (var i in settings)
+		xhr[i] = settings[i]
+
+	for (var i in headers)
+		xhr.setRequestHeader(i, headers[i])
+
+	this.loading = true
+	if (request.data)
+		xhr.send(request.data)
+	else
+		xhr.send()
+}
+
 exports.fingerprint = function(ctx, fingerprint) {
 	var html = exports
 	try {

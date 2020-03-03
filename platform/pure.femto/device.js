58ed4cb19d42535b9a70af49aa56ec4c0497e144
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-03-03 22:03:48 +0000

    register lockOrientation and keepScreenOn

--- platform/pure.femto/device.js
+++ platform/pure.femto/device.js
@@ -2,4 +2,11 @@ exports.createDevice = function(ui) {
 	var info = fd.getDeviceInfo()
 	Object.assign(ui, info)
 	ui._context.system.device = info.device
+	ui.lockOrientation = function(orientation) {
+		fd.setDeviceFeature('orientation', orientation)
+	}
+
+	ui.keepScreenOn = function(enable) {
+		fd.setDeviceFeature('keep-screen-on', enable)
+	}
 }

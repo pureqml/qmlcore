c5e212f9dc10f6712195aa9f0281c785523ea53d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-05-10 17:28:44 +0100

    add stub for pure.blessed backend

---
+++ platform/pure.blessed/.core.js
@@ -0,0 +1,11 @@
+if ((typeof process !== 'undefined') && (process.release.name === 'node')) {
+	exports.core.os = process.platform
+	exports.core.userAgent = process.release.name
+}
+
+_globals._backend = function() { return _globals.pure.blessed.backend }
+_globals.core.__locationBackend = function() { return _globals.pure.blessed.backend }
+_globals.core.__deviceBackend = function() { return _globals.pure.blessed.backend }
+_globals.core.__localStorageBackend = function() { return _globals.pure.blessed.backend }
+
+_globals.core.__videoBackends.void = function() { return _globals.pure.blessed.backend }

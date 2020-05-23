183172c6b10c8196bb1d10b8484b9430919dec14
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2020-05-23 18:36:39 +0100

    check that '_updateVisibilityForChild' is actually in parent before calling it

--- core/core.js
+++ core/core.js
@@ -836,7 +836,8 @@ exports.forEach = function(root, callback, arg) {
 exports.createObject = function(item) {
 	item.__init()
 	var parent = item.parent
-	parent._updateVisibilityForChild(item, parent.recursiveVisible)
+	if ('_updateVisibilityForChild' in parent)
+		parent._updateVisibilityForChild(item, parent.recursiveVisible)
 	if ('_tryFocus' in parent)
 		parent._tryFocus()
 	item._context.scheduleComplete()

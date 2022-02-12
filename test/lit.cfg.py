562368c13bfe70f0dbb30e728702c90eb52bed12
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-02-12 14:03:35 +0000

    remove -E, new parser is used by default now

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -13,5 +13,5 @@ build_path = os.path.join(config.my_obj_root, 'build')
 cache_dir = os.path.join(config.test_exec_root, '.cache')
 manifest = '{"sources":"qml","apps":["%noext_basename_s"],"package":"test"}'
 config.substitutions.append(("%out","%S/../build.%basename_t"))
-config.substitutions.append(('%build', ": rm -f %s/test.%%noext_basename_s && rm -rf build.%%basename_t && cd %s && %s -E -v --build-dir=build.%%basename_t --cache-dir=%s --inline-manifest=\'%s\'" % (cache_dir, config.test_exec_root, build_path, cache_dir, manifest)))
+config.substitutions.append(('%build', ": rm -f %s/test.%%noext_basename_s && rm -rf build.%%basename_t && cd %s && %s -v --build-dir=build.%%basename_t --cache-dir=%s --inline-manifest=\'%s\'" % (cache_dir, config.test_exec_root, build_path, cache_dir, manifest)))
 

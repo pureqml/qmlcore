a49273bc05cbc279bf87e22624af1a6394ee1413
Alexey Pobery <poberyaa@gmail.com>
2022-10-23 02:08:25 +0300

    Actualize the ios building script

--- platform/ios/build.py
+++ platform/ios/build.py
@@ -7,28 +7,22 @@ import os
 
 
 def build(app, title):
+	os.system('ls ./')
 	os.system('rm -rf %s' %app)
-	res = os.system('cordova create %s com.%s.app %s' %(app, app, title))
+	res = os.system('cordova create %s com.%s.app %s' %(title, app, title))
 	if res != 0:
 		print("Failed to create ios app")
 		return
 	os.system('rsync -a ./ %s/www --exclude=%s ' %(app,app))
-	# os.system('cp icon.png %s' %(app))
 	os.system('cp config.xml %s' %(app))
+
 	os.chdir(app)
-
 	os.system('cordova platform add ios@5.1.0')
-	{% block commands %}{% endblock %}
-
 	os.system('cordova plugin add cordova-plugin-device')
 	os.system('cordova plugin add cordova-plugin-screen-orientation')
 	os.system('cordova plugin add cordova-plugin-wkwebview-engine')
-	{% block plugins %}{% endblock %}
-
 	os.system('cordova build ios')
-
 	os.system('cp -r ../icons/* ./platforms/ios/%s/Images.xcassets/AppIcon.appiconset' %(title))
-
 	os.system('cordova run --ios')
 	os.chdir('..')
 
@@ -41,6 +35,6 @@ args = parser.parse_args()
 
 res = os.system('cordova --version')
 if res == 0:
-    build(args.app, args.title)
+	build(args.app, args.title)
 else:
-    print('Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/')
+	print('Install "cordova" first: https://cordova.apache.org/docs/en/latest/guide/cli/')

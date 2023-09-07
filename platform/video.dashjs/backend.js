6c6151c54eb6c9bdf6e4ea1ecf185dad2792347a
Alexey Pobery <poberyaa@gmail.com>
2023-09-07 18:44:10 +0300

    Implement 'setupDrm' for the dashjs platform

--- platform/video.dashjs/backend.js
+++ platform/video.dashjs/backend.js
@@ -7,8 +7,7 @@ var Player = function(ui) {
 	this.ui = ui
 	this.setEventListeners()
 
-	if (ui.element)
-		ui.element.remove()
+	ui.element.remove()
 	ui.element = player
 	ui.parent.element.append(ui.element)
 
@@ -19,32 +18,44 @@ var Player = function(ui) {
 Player.prototype = Object.create(_globals.video.html5.backend.Player.prototype)
 
 Player.prototype.setSource = function(url) {
+	log("dashjs::setSource", url)
 	this.ui.ready = false
 	this.dash.attachSource(url)
+
 	if (this.ui.autoPlay)
 		this.play()
 }
 
 Player.prototype.setupDrm = function(type, options, callback, error) {
-	var drmData = {}
+	var drmConfig = {}
 	if (type === "widevine") {
-		drmData["com.widevine.alpha"] = { "serverURL": options.laServer }
-		drmData["priority"] = 1
+		drmConfig["com.widevine.alpha"] = {
+			"serverURL": options.laServer,
+			"systemStringPriority": ["com.widevine.something", "com.widevine.alpha"],
+			"priority": 1
+		}
+	} else if (type === "playready") {
+		drmConfig["com.microsoft.playready"] = {
+			"serverURL": options.laServer,
+			"priority": 1,
+			"systemStringPriority": ["com.microsoft.playready.something", "com.microsoft.playready.recommendation", "com.microsoft.playready.hardware", "com.microsoft.playready"]
+		}
 	} else {
 		error ? error(new Error("Unknown or not supported DRM type " + type)) : log("Unknown or not supported DRM type " + type)
 	}
 
-	log("setupDrm", drmData)
-	this.dash.setProtectionData(drmData);
-	callback()
+	this.dash.setProtectionData(drmConfig)
+	if (callback)
+		callback()
 }
+
 
 exports.createPlayer = function(ui) {
 	return new Player(ui)
 }
 
 exports.probeUrl = function(url) {
-	return 100500
+	return 10
 }
 
 Player.prototype.dispose = function() {

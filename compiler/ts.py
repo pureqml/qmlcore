ba67f47722f6e6845e46eacd3bf93418e739bde3
Alexey Pobery <poberyaa@gmail.com>
2024-03-19 10:03:15 +0300

    Use 'str' instead of 'unicode'

--- compiler/ts.py
+++ compiler/ts.py
@@ -39,7 +39,7 @@ class Location(object):
 	def save(self, parent):
 		loc = ET.SubElement(parent, 'location')
 		loc.attrib['filename'] = self.filename
-		loc.attrib['line'] = unicode(self.line)
+		loc.attrib['line'] = str(self.line)
 
 class Translation(object):
 	def __init__(self, type = None, text = None):

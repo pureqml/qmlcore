db3d72cdaf2ee98b336816209b971cbca45884d8
Alexey Pobery <poberyaa@gmail.com>
2026-09-24 12:04:32 +0300

    Actualize lang

--- compiler/lang.py
+++ compiler/lang.py
@@ -4,6 +4,8 @@ from past.builtins import basestring
 
 import re
 
+trivial_value_re = re.compile(r'\$\{manifest\.[a-zA-Z0-9\$\.]+\}\Z')
+
 def value_is_trivial(value):
 	if isinstance(value, bool):
 		return True
@@ -16,10 +18,10 @@ def value_is_trivial(value):
 	if value[0] == '(' and value[-1] == ')':
 		value = value[1:-1]
 
-	if value == 'true' or value == 'false' or value == 'null':
+	if value == 'true' or value == 'false' or value == 'null' or value == 'undefined' or value == 'this':
 		return True
 
-	if value.startswith('${manifest.'):
+	if trivial_value_re.match(value):
 		return True
 
 	try:
@@ -33,57 +35,97 @@ def value_is_trivial(value):
 	#print "?trivial", value
 	return False
 
+class Entity(object):
+	__slots__ = ('doc', 'loc')
+	def __init__(self, doc = None, loc = None):
+		self.doc, self.loc = doc, loc
+
 def to_string(value):
 	if isinstance(value, (str, basestring)):
 		return value
 	elif isinstance(value, bool):
 		return 'true' if value else 'false'
-	elif isinstance(value, object):
+	elif isinstance(value, Entity):
 		return value
 	else:
 		return str(value)
+
+re_x = re.compile(r'\\x([0-9a-f]{2})', re.I)
+re_u = re.compile(r'\\u([0-9a-f]{4})', re.I)
+re_o = re.compile(r'\\([0-7]{3})')
+re_u2 = re.compile(r'\\([0-9][0-9a-f]{0,3})', re.I) #old js unicode stuff, try \07f
+re_esc = re.compile(r'\\(.)')
+
+def unescape_string(value):
+	value = re_x.sub(lambda m: chr(int(m.group(1), 16)), value)
+	value = re_u.sub(lambda m: chr(int(m.group(1), 16)), value)
+	value = re_o.sub(lambda m: chr(int(m.group(1), 8)), value)
+	value = re_u2.sub(lambda m: chr(int(m.group(1), 16)), value)
+
+	def unescape(m):
+		c = m.group(1)
+		return {
+			'0': '\0',
+			'n': '\n',
+			'r': '\r',
+			'v': '\v',
+			't': '\t',
+			'b': '\b',
+			'f': '\f',
+		}.get(c, c)
+
+	value = re_esc.sub(unescape, value)
+	return value
 
 def handle_property_path(t):
 	return '${%s}' %t
 
 class DocumentationString(object):
+	__slots__ = 'text'
 	def __init__(self, text):
 		self.text = text
 
-class Entity(object):
-	def __init__(self):
-		self.doc = None
-
 class Component(Entity):
-	def __init__(self, name, children):
-		super(Component, self).__init__()
+	__slots__ = 'name', 'children'
+	def __init__(self, name, children, **kw):
+		super(Component, self).__init__(**kw)
 		self.name = name
 		self.children = children
 
 class Property(Entity):
-	def __init__(self, type, properties):
-		super(Property, self).__init__()
+	__slots__ = 'lazy', 'const', 'type', 'properties'
+	def __init__(self, type, properties, **kw):
+		super(Property, self).__init__(**kw)
 		self.lazy = type == 'lazy'
 		self.const = type == 'const'
 		self.type = type
 		self.properties = properties
 
+class Const(Entity):
+	__slots__ = 'type', 'name', 'value'
+	def __init__(self, type, name, value, **kw):
+		super(Const, self).__init__(**kw)
+		self.type, self.name, self.value = type, name, value
+
 class AliasProperty(Entity):
-	def __init__(self, name, target):
-		super(AliasProperty, self).__init__()
+	__slots__ = 'name', 'target'
+	def __init__(self, name, target, **kw):
+		super(AliasProperty, self).__init__(**kw)
 		self.name = name
 		self.target = target
 
 class EnumProperty(Entity):
-	def __init__(self, name, values, default):
-		super(EnumProperty, self).__init__()
+	__slots__ = 'name', 'values', 'default'
+	def __init__(self, name, values, default, **kw):
+		super(EnumProperty, self).__init__(**kw)
 		self.name = name
 		self.values = values
 		self.default = default
 
 class Method(Entity):
-	def __init__(self, name, args, code, event, async_):
-		super(Method, self).__init__()
+	__slots__ = 'name', 'args', 'code', 'event', 'async_'
+	def __init__(self, name, args, code, event, async_, **kw):
+		super(Method, self).__init__(**kw)
 		self.name = name
 		self.args = args
 		self.code = code
@@ -91,50 +133,45 @@ class Method(Entity):
 		self.async_ = async_
 
 class IdAssignment(Entity):
-	def __init__(self, name):
-		super(IdAssignment, self).__init__()
+	__slots__ = 'name'
+	def __init__(self, name, **kw):
+		super(IdAssignment, self).__init__(**kw)
 		self.name = name
 
 class Assignment(Entity):
-	re_name = re.compile('<property-name>')
-
-	def __init__(self, target, value):
-		super(Assignment, self).__init__()
+	__slots__ = 'target', 'value'
+	def __init__(self, target, value, **kw):
+		super(Assignment, self).__init__(**kw)
 		self.target = target
-
-		dot = target.rfind('.')
-		property_name = target[dot + 1:] if dot >= 0 else target
-		if property_name == 'x':
-			property_name = 'width'
-		elif property_name == 'y':
-			property_name = 'height'
-
-		if isinstance(value, (str, basestring)):
-			self.value = Assignment.re_name.sub(property_name, value)
-		else:
-			self.value = to_string(value)
+		if not isinstance(value, (str, basestring)):
+			value = to_string(value)
+		self.value = value
 
 	def is_trivial(self):
 		return value_is_trivial(self.value)
 
 class AssignmentScope(Entity):
-	def __init__(self, target, values):
-		super(AssignmentScope, self).__init__()
+	__slots__ = 'target', 'values'
+	def __init__(self, target, values, **kw):
+		super(AssignmentScope, self).__init__(**kw)
 		self.target = target
 		self.values = values
 
 class Behavior(Entity):
-	def __init__(self, target, animation):
-		super(Behavior, self).__init__()
+	__slots__ = 'target', 'animation'
+	def __init__(self, target, animation, **kw):
+		super(Behavior, self).__init__(**kw)
 		self.target = target
 		self.animation = animation
 
 class Signal(Entity):
-	def __init__(self, name):
-		super(Signal, self).__init__()
+	__slots__ = 'name'
+	def __init__(self, name, **kw):
+		super(Signal, self).__init__(**kw)
 		self.name = name
 
 class ListElement(Entity):
-	def __init__(self, data):
-		super(ListElement, self).__init__()
+	__slots__ = 'data'
+	def __init__(self, data, **kw):
+		super(ListElement, self).__init__(**kw)
 		self.data = data
